b54b83b5556f3b8e0923cd82108c4f0dd3d8c2d4
ostibilas <lambgust@gmail.com>
2025-11-26 21:46:51 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const {produtoRoutes} = require("./src/routes/produtoRoutes");
+const {clienteRoutes} = require("./src/routes/clienteRoutes");
+const PORT = 8081;
+
+app.use(express.json());// configurando o midleway para aceitar o json
+app.use('/', produtoRoutes);
+app.use('/', clienteRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+//teste
